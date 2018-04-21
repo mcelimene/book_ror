9f70894647df21f5ce6c4d8a754c50f3e2ba7562
Mélodie <melodie.celimene@yahoo.fr>
2018-04-21 17:54:33 +0200

    Searching and Finding with Queries

---
+++ judebox/db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Album.create(name: "Stg. Pepper's Lonelt Hearts Club Band", release_year: 1967)
+Album.create(name: "Pet Sounds", release_year: 1966)
+Album.create(name: "Revolver", release_year: 1966)
+Album.create(name: "Highway 61 Revisited", release_year: 1965)
+Album.create(name: "Rubber Soul", release_year: 1965)
+Album.create(name: "What's Going on", release_year: 1971)
+Album.create(name: "Exile on Main St.", release_year: 1972)
+Album.create(name: "London Calling", release_year: 1979)
+Album.create(name: "Blonde on Blonde", release_year: 1966)
+Album.create(name: "The Beatles", release_year: 1968)
