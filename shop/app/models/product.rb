5f5525b15aaead1c50a9f95398a072e744e0a46e
Mélodie <melodie.celimene@yahoo.fr>
2018-04-29 23:45:23 +0200

    => defining a scope

--- shop/app/models/product.rb
+++ shop/app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+	# Associations
 	has_many :line_items
 	has_many :orders, through: :line_items
+
+	#Scopes
+	scope :available, -> {where(in_stock: true)}
+	scope :cheap, -> {where(price: 0..1)}
 end
