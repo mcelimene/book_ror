581ea37aa3614b33d9f380a6e771dd9d69ef3b60
Mélodie <melodie.celimene@yahoo.fr>
2018-04-29 23:49:12 +0200

    => passing in arguments

--- shop/app/models/product.rb
+++ shop/app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
 	#Scopes
 	scope :available, -> {where(in_stock: true)}
 	scope :cheap, -> {where(price: 0..1)}
+	scope :cheap_than, -> (price) {where("price < ?", price)}
 end
