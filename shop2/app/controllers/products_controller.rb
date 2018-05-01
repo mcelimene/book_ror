016c8ca6cc8abd3ac1435e4c985292e76a860bbd
Mélodie <melodie.celimene@yahoo.fr>
2018-05-01 19:36:58 +0200

    => json

--- shop2/app/controllers/products_controller.rb
+++ shop2/app/controllers/products_controller.rb
@@ -5,6 +5,12 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @products }
+      format.xml { render xml: @products }
+    end
   end
 
   # GET /products/1
